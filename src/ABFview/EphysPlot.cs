6d7ef622d3240ee4e93e07faa4f8a7239d08fc1a
Scott W Harden <swharden@gmail.com>
2022-06-13 18:50:27 -0400

    use .NET 6 features
    capitalize property names, remove unnecessary usings, use top-level namespaces

--- src/ABFview/EphysPlot.cs
+++ src/ABFview/EphysPlot.cs
@@ -1,88 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Linq;
 
-namespace ABFview
-{
-    public static class EphysPlot
-    {
-        public static void Sweep(ScottPlot.Plot plt, AbfSharp.ABF abf, int sweepNumber, bool derivative)
-        {
-            plt.Clear();
+namespace ABFview;
 
-            plt.AddSignal(
-                ys: GetSweepValues(abf, sweepNumber, derivative),
+public static class EphysPlot
+{
+    public static void Sweep(ScottPlot.Plot plt, AbfSharp.ABF abf, int sweepNumber, bool derivative)
+    {
+        plt.Clear();
+
+        plt.AddSignal(
+            ys: GetSweepValues(abf, sweepNumber, derivative),
+            sampleRate: abf.Header.SampleRate,
+            color: derivative ? Color.Red : Color.Blue);
+
+        plt.Title($"Sweep {sweepNumber + 1} of {abf.Header.SweepCount}");
+        plt.XLabel("Sweep Time (seconds)");
+        plt.AxisAuto(0, .1);
+    }
+
+    public static void Stack(ScottPlot.Plot plt, AbfSharp.ABF abf, double yOffset, bool derivative)
+    {
+        plt.Clear();
+
+        for (int i = 0; i < abf.Header.SweepCount; i++)
+        {
+            var sig = plt.AddSignal(
+                ys: GetSweepValues(abf, i, derivative),
                 sampleRate: abf.Header.SampleRate,
                 color: derivative ? Color.Red : Color.Blue);
 
-            plt.Title($"Sweep {sweepNumber + 1} of {abf.Header.SweepCount}");
-            plt.XLabel("Sweep Time (seconds)");
-            plt.AxisAuto(0, .1);
+            sig.OffsetY = i * yOffset;
         }
 
-        public static void Stack(ScottPlot.Plot plt, AbfSharp.ABF abf, double yOffset, bool derivative)
+        plt.Title($"Stacked Sweeps");
+        plt.XLabel("Sweep Time (seconds)");
+        plt.AxisAuto(0, .1);
+    }
+
+    public static void Full(ScottPlot.Plot plt, AbfSharp.ABF abf, bool derivative)
+    {
+        plt.Clear();
+
+        plt.AddSignal(
+            ys: GetAllSweepValues(abf, derivative),
+            abf.Header.SampleRate * 60,
+            color: derivative ? Color.Red : Color.Blue);
+
+        plt.XLabel("Sweep Time (minutes)");
+        plt.Title($"Full Recording");
+        plt.AxisAuto(0, .1);
+    }
+
+    private static double[] Diff(double[] source, int deltaPoints = 1)
+    {
+        double[] deriv = new double[source.Length];
+        for (int i = deltaPoints; i < source.Length; i++)
+            deriv[i] = source[i] - source[i - deltaPoints];
+        for (int i = 0; i < deltaPoints; i++)
+            deriv[i] = deriv[deltaPoints];
+        return deriv;
+    }
+
+    private static double[] GetSweepValues(AbfSharp.ABF abf, int sweepNumber, bool derivative)
+    {
+        float[] valuesRaw = abf.GetSweep(sweepNumber);
+        double[] values = new double[valuesRaw.Length];
+        for (int i = 0; i < valuesRaw.Length; i++)
+            values[i] = valuesRaw[i];
+        return derivative ? Diff(values) : values;
+    }
+
+    private static double[] GetAllSweepValues(AbfSharp.ABF abf, bool derivative)
+    {
+        double[][] values = new double[abf.Header.SweepCount][];
+        for (int i = 0; i < abf.Header.SweepCount; i++)
         {
-            plt.Clear();
-
-            for (int i = 0; i < abf.Header.SweepCount; i++)
-            {
-                var sig = plt.AddSignal(
-                    ys: GetSweepValues(abf, i, derivative),
-                    sampleRate: abf.Header.SampleRate,
-                    color: derivative ? Color.Red : Color.Blue);
-
-                sig.OffsetY = i * yOffset;
-            }
-
-            plt.Title($"Stacked Sweeps");
-            plt.XLabel("Sweep Time (seconds)");
-            plt.AxisAuto(0, .1);
+            values[i] = GetSweepValues(abf, i, derivative);
         }
 
-        public static void Full(ScottPlot.Plot plt, AbfSharp.ABF abf, bool derivative)
-        {
-            plt.Clear();
-
-            plt.AddSignal(
-                ys: GetAllSweepValues(abf, derivative),
-                abf.Header.SampleRate * 60,
-                color: derivative ? Color.Red : Color.Blue);
-
-            plt.XLabel("Sweep Time (minutes)");
-            plt.Title($"Full Recording");
-            plt.AxisAuto(0, .1);
-        }
-
-        private static double[] Diff(double[] source, int deltaPoints = 1)
-        {
-            double[] deriv = new double[source.Length];
-            for (int i = deltaPoints; i < source.Length; i++)
-                deriv[i] = source[i] - source[i - deltaPoints];
-            for (int i = 0; i < deltaPoints; i++)
-                deriv[i] = deriv[deltaPoints];
-            return deriv;
-        }
-
-        private static double[] GetSweepValues(AbfSharp.ABF abf, int sweepNumber, bool derivative)
-        {
-            float[] valuesRaw = abf.GetSweep(sweepNumber);
-            double[] values = new double[valuesRaw.Length];
-            for (int i = 0; i < valuesRaw.Length; i++)
-                values[i] = valuesRaw[i];
-            return derivative ? Diff(values) : values;
-        }
-
-        private static double[] GetAllSweepValues(AbfSharp.ABF abf, bool derivative)
-        {
-            double[][] values = new double[abf.Header.SweepCount][];
-            for (int i = 0; i < abf.Header.SweepCount; i++)
-            {
-                values[i] = GetSweepValues(abf, i, derivative);
-            }
-
-            return values.SelectMany(a => a).ToArray();
-        }
+        return values.SelectMany(a => a).ToArray();
     }
 }
--- src/ABFview/MainWindow.xaml.cs
+++ src/ABFview/MainWindow.xaml.cs
@@ -1,227 +1,204 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ABFview
+namespace ABFview;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    AbfSharp.ABF Abf;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            string demoAbfFolder = System.IO.Path.GetFullPath("../../../../../dev/abfs/");
-            string demoAbfFile = System.IO.Path.Combine(demoAbfFolder, "ic-ramp-ap.abf");
+        gbSweepNav.Visibility = Visibility.Collapsed;
+        gbStackSettings.Visibility = Visibility.Collapsed;
+        gbView.Visibility = Visibility.Collapsed;
+        btnNextAbf.Visibility = Visibility.Collapsed;
+        btnPreviousAbf.Visibility = Visibility.Collapsed;
 
-            if (IntPtr.Size != 4)
-                throw new Exception("Must build as 32-bit (x86)");
+        wpfPlot1.Plot.AxisAuto();
+        wpfPlot1.Render();
 
-            gbSweepNav.Visibility = Visibility.Collapsed;
-            gbStackSettings.Visibility = Visibility.Collapsed;
-            gbView.Visibility = Visibility.Collapsed;
-            btnNextAbf.Visibility = Visibility.Collapsed;
-            btnPreviousAbf.Visibility = Visibility.Collapsed;
-
-            wpfPlot1.Plot.AxisAuto();
-            wpfPlot1.Render();
-
+        string demoAbfFolder = System.IO.Path.GetFullPath("../../../../../dev/abfs/");
+        string demoAbfFile = System.IO.Path.Combine(demoAbfFolder, "ic-ramp-ap.abf");
+        if (System.IO.File.Exists(demoAbfFile))
             LoadAbf(demoAbfFile);
-        }
+    }
 
-        AbfSharp.ABF abf;
-        private void LoadAbf(string filePath)
+    private void LoadAbf(string filePath)
+    {
+        Abf = new AbfSharp.ABF(filePath);
+
+        Title = $"ABFview {System.IO.Path.GetFileName(filePath)}";
+        SetViewMode("sweep");
+        SetSweep(0);
+
+        gbView.Visibility = Visibility.Visible;
+        btnNextAbf.Visibility = Visibility.Visible;
+        btnPreviousAbf.Visibility = Visibility.Visible;
+    }
+
+    int currentSweep = 0;
+
+    private void SetSweep(int sweepNumber = 1)
+    {
+        currentSweep = sweepNumber;
+        EphysPlot.Sweep(wpfPlot1.Plot, Abf, sweepNumber, (bool)cbDerivative.IsChecked);
+        wpfPlot1.Render();
+        lblSweep.Content = $"{sweepNumber + 1} of {Abf.Header.SweepCount}";
+    }
+
+    private void SetStack()
+    {
+        if (!double.TryParse(tbVertSep.Text, out double yOffset))
+            yOffset = 0;
+
+        EphysPlot.Stack(wpfPlot1.Plot, Abf, yOffset, (bool)cbDerivative.IsChecked);
+        wpfPlot1.Render();
+    }
+
+    private void SetFull()
+    {
+        EphysPlot.Full(wpfPlot1.Plot, Abf, (bool)cbDerivative.IsChecked);
+        wpfPlot1.Render();
+    }
+
+    private void btnSweepPrevious_Click(object sender, RoutedEventArgs e)
+    {
+        SetSweep(Math.Max(0, currentSweep - 1));
+    }
+
+    private void btnSweepNext_Click(object sender, RoutedEventArgs e)
+    {
+        SetSweep(Math.Min(Abf.Header.SweepCount - 1, currentSweep + 1));
+    }
+
+    private void btnSweepFirst_Click(object sender, RoutedEventArgs e)
+    {
+        SetSweep(0);
+    }
+
+    private void btnSweepLast_Click(object sender, RoutedEventArgs e)
+    {
+        SetSweep(Abf.Header.SweepCount - 1);
+    }
+
+    private void cbDelta_CheckChanged(object sender, RoutedEventArgs e)
+    {
+        double x1 = wpfPlot1.Plot.GetAxisLimits().XMin;
+        double x2 = wpfPlot1.Plot.GetAxisLimits().XMax;
+        SetViewMode();
+        wpfPlot1.Plot.SetAxisLimits(xMin: x1, xMax: x2);
+        wpfPlot1.Render();
+    }
+
+    private void SetViewMode(string viewMode = null)
+    {
+        if (wpfPlot1 is null)
+            return;
+
+        if (viewMode is null)
+            viewMode = cbView.Text;
+
+        // start by hiding all custom panels
+        gbSweepNav.Visibility = Visibility.Collapsed;
+        gbStackSettings.Visibility = Visibility.Collapsed;
+
+        // reveal relevant panels only
+        switch (viewMode)
         {
-            if (!System.IO.File.Exists(filePath))
-                return;
+            case "sweep":
+                SetSweep();
+                gbSweepNav.Visibility = Visibility.Visible;
+                break;
 
-            if (abf != null)
-            {
-                Debug.WriteLine($"Closing old ABF...");
-            }
+            case "stack":
+                SetStack();
+                gbStackSettings.Visibility = Visibility.Visible;
+                break;
 
-            Debug.WriteLine($"Loading ABF: {filePath}");
-            abf = new AbfSharp.ABF(filePath);
-            Title = $"ABFview {System.IO.Path.GetFileName(filePath)}";
-            SetViewMode("sweep");
-            SetSweep(0);
+            case "full":
+                SetFull();
+                break;
 
-            gbView.Visibility = Visibility.Visible;
-            btnNextAbf.Visibility = Visibility.Visible;
-            btnPreviousAbf.Visibility = Visibility.Visible;
+            default:
+                throw new NotImplementedException();
         }
+    }
 
-        int currentSweep = 0;
+    private void cbView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        string viewMode = ((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string;
+        if (wpfPlot1 != null && viewMode != null)
+            SetViewMode(viewMode);
+    }
 
-        private void SetSweep(int sweepNumber = 1)
+    private void btnLoadAbf_Click(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFileDialog openFileDlg = new();
+        openFileDlg.DefaultExt = ".abf";
+        openFileDlg.Filter = "ABF Files|*.abf";
+        if (openFileDlg.ShowDialog() == true)
         {
-            currentSweep = sweepNumber;
-            EphysPlot.Sweep(wpfPlot1.Plot, abf, sweepNumber, (bool)cbDerivative.IsChecked);
-            wpfPlot1.Render();
-            lblSweep.Content = $"{sweepNumber + 1} of {abf.Header.SweepCount}";
+            string filePath = System.IO.Path.GetFullPath(openFileDlg.FileName);
+            LoadAbf(filePath);
         }
+    }
 
-        private void SetStack()
+    private void tbVertSep_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        SetViewMode();
+    }
+
+    private string GetPathAdjacentAbf(string pathAbf, bool nextAbf = true)
+    {
+        pathAbf = System.IO.Path.GetFullPath(pathAbf);
+        string abfFolder = System.IO.Path.GetDirectoryName(pathAbf);
+
+        string[] abfsInFolder = System.IO.Directory.GetFiles(abfFolder, "*.abf");
+        Array.Sort(abfsInFolder);
+        for (int i = 0; i < abfsInFolder.Length; i++)
+            abfsInFolder[i] = abfsInFolder[i].ToUpper();
+
+        int index = Array.IndexOf(abfsInFolder, pathAbf.ToUpper());
+
+        if (nextAbf)
+            index += 1;
+        else
+            index -= 1;
+
+        if ((index >= 0) && (index < abfsInFolder.Length))
+            return abfsInFolder[index];
+        else
+            return null;
+    }
+
+    private void btnPreviousAbf_Click(object sender, RoutedEventArgs e)
+    {
+        string previousAbf = GetPathAdjacentAbf(Abf.Path, nextAbf: false);
+        if (previousAbf != null)
+            LoadAbf(previousAbf);
+    }
+
+    private void btnNextAbf_Click(object sender, RoutedEventArgs e)
+    {
+        string nextAbf = GetPathAdjacentAbf(Abf.Path, nextAbf: true);
+        if (nextAbf != null)
+            LoadAbf(nextAbf);
+    }
+
+    private void OnDrop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
         {
-            if (!double.TryParse(tbVertSep.Text, out double yOffset))
-                yOffset = 0;
-
-            EphysPlot.Stack(wpfPlot1.Plot, abf, yOffset, (bool)cbDerivative.IsChecked);
-            wpfPlot1.Render();
-        }
-
-        private void SetFull()
-        {
-            EphysPlot.Full(wpfPlot1.Plot, abf, (bool)cbDerivative.IsChecked);
-            wpfPlot1.Render();
-        }
-
-        private void btnSweepPrevious_Click(object sender, RoutedEventArgs e)
-        {
-            SetSweep(Math.Max(0, currentSweep - 1));
-        }
-
-        private void btnSweepNext_Click(object sender, RoutedEventArgs e)
-        {
-            SetSweep(Math.Min(abf.Header.SweepCount - 1, currentSweep + 1));
-        }
-
-        private void btnSweepFirst_Click(object sender, RoutedEventArgs e)
-        {
-            SetSweep(0);
-        }
-
-        private void btnSweepLast_Click(object sender, RoutedEventArgs e)
-        {
-            SetSweep(abf.Header.SweepCount - 1);
-        }
-
-        private void cbDelta_CheckChanged(object sender, RoutedEventArgs e)
-        {
-            double x1 = wpfPlot1.Plot.GetAxisLimits().XMin;
-            double x2 = wpfPlot1.Plot.GetAxisLimits().XMax;
-            SetViewMode();
-            wpfPlot1.Plot.SetAxisLimits(xMin: x1, xMax: x2);
-            wpfPlot1.Render();
-        }
-
-        private void SetViewMode(string viewMode = null)
-        {
-            if (wpfPlot1 is null)
-                return;
-
-            if (viewMode is null)
-                viewMode = cbView.Text;
-
-            // start by hiding all custom panels
-            gbSweepNav.Visibility = Visibility.Collapsed;
-            gbStackSettings.Visibility = Visibility.Collapsed;
-
-            // reveal relevant panels only
-            switch (viewMode)
-            {
-                case "sweep":
-                    SetSweep();
-                    gbSweepNav.Visibility = Visibility.Visible;
-                    break;
-
-                case "stack":
-                    SetStack();
-                    gbStackSettings.Visibility = Visibility.Visible;
-                    break;
-
-                case "full":
-                    SetFull();
-                    break;
-
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        private void cbView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string viewMode = ((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string;
-            if (wpfPlot1 != null && viewMode != null)
-                SetViewMode(viewMode);
-        }
-
-        private void btnLoadAbf_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
-            openFileDlg.DefaultExt = ".abf";
-            openFileDlg.Filter = "ABF Files|*.abf";
-            if (openFileDlg.ShowDialog() == true)
-            {
-                string filePath = System.IO.Path.GetFullPath(openFileDlg.FileName);
-                LoadAbf(filePath);
-            }
-        }
-
-        private void tbVertSep_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            SetViewMode();
-        }
-
-        private string GetPathAdjacentAbf(string pathAbf, bool nextAbf = true)
-        {
-            pathAbf = System.IO.Path.GetFullPath(pathAbf);
-            string abfFolder = System.IO.Path.GetDirectoryName(pathAbf);
-
-            string[] abfsInFolder = System.IO.Directory.GetFiles(abfFolder, "*.abf");
-            Array.Sort(abfsInFolder);
-            for (int i = 0; i < abfsInFolder.Length; i++)
-                abfsInFolder[i] = abfsInFolder[i].ToUpper();
-
-            int index = Array.IndexOf(abfsInFolder, pathAbf.ToUpper());
-
-            if (nextAbf)
-                index += 1;
-            else
-                index -= 1;
-
-            if ((index >= 0) && (index < abfsInFolder.Length))
-                return abfsInFolder[index];
-            else
-                return null;
-        }
-
-        private void btnPreviousAbf_Click(object sender, RoutedEventArgs e)
-        {
-            string previousAbf = GetPathAdjacentAbf(abf.Path, nextAbf: false);
-            if (previousAbf != null)
-                LoadAbf(previousAbf);
-        }
-
-        private void btnNextAbf_Click(object sender, RoutedEventArgs e)
-        {
-            string nextAbf = GetPathAdjacentAbf(abf.Path, nextAbf: true);
-            if (nextAbf != null)
-                LoadAbf(nextAbf);
-        }
-
-        private void OnDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                if (files[0].ToUpper().EndsWith("ABF"))
-                    LoadAbf(files[0]);
-            }
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files[0].ToUpper().EndsWith("ABF"))
+                LoadAbf(files[0]);
         }
     }
 }
