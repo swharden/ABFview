b5279f4e574467c1db070eceb00222bc42a7e1af
Scott W Harden <swharden@gmail.com>
2022-06-13 20:14:28 -0400

    dev: create build script

---
+++ dev/package.bat
@@ -0,0 +1,9 @@
+rmdir /s /q ..\src\ABFview\bin\
+dotnet publish ../src
+cd ..\src\ABFview\bin\Debug\net6.0-windows
+rename publish ABFview
+tar -a -cf ABFview-1.X.zip ABFview
+move ABFview-1.X.zip ..\
+cd ..\
+explorer .
+pause
