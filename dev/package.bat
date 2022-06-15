e46680e6a1de27d1daf91750a25146625dc7afb9
Scott W Harden <swharden@gmail.com>
2022-06-14 23:46:57 -0400

    dev: update build script

--- dev/package.bat
+++ dev/package.bat
@@ -3,7 +3,7 @@ dotnet publish ../src
 cd ..\src\ABFview\bin\Debug\net6.0-windows
 rename publish ABFview
 tar -a -cf ABFview-1.X.zip ABFview
-move ABFview-1.X.zip ..\
-cd ..\
+move ABFview-1.X.zip ..\..\..\..\..\website\download
+cd ..\..\..\..\..\website\download
 explorer .
 pause
